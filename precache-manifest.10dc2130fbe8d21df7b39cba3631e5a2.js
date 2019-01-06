3a29c031434ed04c4df56d94094e4118496e2db0
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-05 19:58:10 -0500

    Updates

---
+++ precache-manifest.10dc2130fbe8d21df7b39cba3631e5a2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbf7159165fb5502152a046ae7e78acc",
+    "url": "/memory-game/static/media/oz.bbf71591.jpg"
+  },
+  {
+    "revision": "a2dbaf308097c0fc96e2",
+    "url": "/memory-game/static/css/main.f11a644d.chunk.css"
+  },
+  {
+    "revision": "5b90219249c77c2e9b0a",
+    "url": "/memory-game/static/js/1.5b902192.chunk.js"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "7f96182e8287ff34b610e564f688405e",
+    "url": "/memory-game/static/media/comedian.7f96182e.jpg"
+  },
+  {
+    "revision": "fd73456815396ae21abd038d8dfacda6",
+    "url": "/memory-game/static/media/hangman.fd734568.jpg"
+  },
+  {
+    "revision": "64b837740f3d7ab2d5fb1a1baa51f61c",
+    "url": "/memory-game/static/media/manhattan-sphere.64b83774.jpg"
+  },
+  {
+    "revision": "bf4cf5d123a2d4804f966b831f9fb6eb",
+    "url": "/memory-game/static/media/manhattan.bf4cf5d1.jpg"
+  },
+  {
+    "revision": "c1cac184c909d2755de3c8beed7f4c1e",
+    "url": "/memory-game/static/media/minutemen.c1cac184.jpg"
+  },
+  {
+    "revision": "97aded37018d7cca45b537e502370f74",
+    "url": "/memory-game/static/media/niteowl.97aded37.jpg"
+  },
+  {
+    "revision": "a2dbaf308097c0fc96e2",
+    "url": "/memory-game/static/js/main.a2dbaf30.chunk.js"
+  },
+  {
+    "revision": "f78617fb76237f78b0672f7f206d5068",
+    "url": "/memory-game/static/media/rorschach.f78617fb.png"
+  },
+  {
+    "revision": "842a5ad6b62f76fbd81dc3e33f299184",
+    "url": "/memory-game/static/media/rorschach.842a5ad6.jpg"
+  },
+  {
+    "revision": "65f363d7d78d95c8a2bf6dcff3c41fb4",
+    "url": "/memory-game/static/media/silkspectre.65f363d7.jpg"
+  },
+  {
+    "revision": "f7981863799e9b4477dfa4464de9429b",
+    "url": "/memory-game/static/media/smiley.f7981863.png"
+  },
+  {
+    "revision": "e9020f61820f539e8b35fc58c48bd6d2",
+    "url": "/memory-game/static/media/watchmen.e9020f61.jpg"
+  },
+  {
+    "revision": "c03152b20d7474e3a5fc88e0e1b3e934",
+    "url": "/memory-game/static/media/watchmen2.c03152b2.jpg"
+  },
+  {
+    "revision": "585dd5a2920e26a1b5fdcdda090cb4d9",
+    "url": "/memory-game/static/media/comedian-dies.585dd5a2.jpg"
+  },
+  {
+    "revision": "1ef8231e8272b434524a029accb4927a",
+    "url": "/memory-game/static/media/ozymandias-thumb.1ef8231e.jpg"
+  },
+  {
+    "revision": "90fc8f9058e433f2101d0b11a2ede33d",
+    "url": "/memory-game/static/media/watchmen-movie.90fc8f90.jpg"
+  },
+  {
+    "revision": "7fec8fa485e02d05b0fffa2fbf2da06d",
+    "url": "/memory-game/index.html"
+  }
+];
